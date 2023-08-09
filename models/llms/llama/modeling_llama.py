24680d1107d6c6f4e4b8b89499bef825d527a742
zjn <zjn@bytedance.com>
2023-08-09 11:16:35 +0800

    update

--- models/llms/llama/modeling_llama.py
+++ models/llms/llama/modeling_llama.py
@@ -250,111 +250,13 @@ class LlamaAttention(nn.Module):
         return attn_output, attn_weights, past_key_value
 
 
-class LlamaFlashAttention(nn.Module):
-    """Multi-headed attention from 'Attention Is All You Need' paper"""
-
-    def __init__(self, config: LlamaConfig):
-        super().__init__()
-        self.config = config
-        self.hidden_size = config.hidden_size
-        self.num_heads = config.num_attention_heads
-        self.head_dim = self.hidden_size // self.num_heads
-        self.max_position_embeddings = config.max_position_embeddings
-
-        if (self.head_dim * self.num_heads) != self.hidden_size:
-            raise ValueError(
-                f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
-                f" and `num_heads`: {self.num_heads})."
-            )
-        self.q_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=False)
-        self.k_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=False)
-        self.v_proj = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=False)
-        self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size, bias=False)
-        self.rotary_emb = LlamaRotaryEmbedding(self.head_dim, max_position_embeddings=self.max_position_embeddings)
-
-        from models.xperf_training import FTFlashAttention
-
-        self.ft_flash_attn = FTFlashAttention()
-
-    def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
-        return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
-
-    def forward(
-        self,
-        hidden_states: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        past_key_value: Optional[Tuple[torch.Tensor]] = None,
-        output_attentions: bool = False,
-        use_cache: bool = False,
-    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-        bsz, q_len, _ = hidden_states.size()
-
-        query_states = self.q_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        key_states = self.k_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        value_states = self.v_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-
-        kv_seq_len = key_states.shape[-2]
-        if past_key_value is not None:
-            kv_seq_len += past_key_value[0].shape[-2]
-        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
-        # [bsz, nh, t, hd]
-
-        if past_key_value is not None:
-            # reuse k, v, self_attention
-            key_states = torch.cat([past_key_value[0], key_states], dim=2)
-            value_states = torch.cat([past_key_value[1], value_states], dim=2)
-
-        past_key_value = (key_states, value_states) if use_cache else None
-
-        # flash attention
-        query_states = query_states.transpose(1, 2).view(bsz, q_len, self.num_heads*self.head_dim)
-        key_states = key_states.transpose(1, 2).view(bsz, q_len, self.num_heads*self.head_dim)
-        value_states = value_states.transpose(1, 2).view(bsz, q_len, self.num_heads*self.head_dim)
-
-        if attention_mask is not None:
-            attention_mask = attention_mask.type_as(query_states)
-        attn_output = self.ft_flash_attn(
-            [query_states, key_states, value_states], self.num_heads, attn_mask=attention_mask, causal=True)
-
-        # attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
-
-        # if attention_mask is not None:
-        #     if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
-        #         raise ValueError(
-        #             f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
-        #         )
-        #     attn_weights = attn_weights + attention_mask
-        #     attn_weights = torch.max(attn_weights, torch.tensor(torch.finfo(attn_weights.dtype).min))
-
-        # # upcast attention to fp32
-        # attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-        # attn_output = torch.matmul(attn_weights, value_states)
-
-        if attn_output.size() != (bsz, q_len, self.hidden_size):
-            raise ValueError(
-                f"`attn_output` should be of size {(bsz, q_len, self.hidden_size)}, but is"
-                f" {attn_output.size()}"
-            )
-
-        attn_output = self.o_proj(attn_output)
-
-        if output_attentions:
-            raise NotImplementedError 
-        else:
-            attn_weights = None
-
-        return attn_output, attn_weights, past_key_value
-
-
 class LlamaDecoderLayer(nn.Module):
     def __init__(self, config: LlamaConfig, use_adapter=False):
         super().__init__()
 
         self.hidden_size = config.hidden_size
 
-        self.self_attn = LlamaFlashAttention(config=config) if config.use_flash_attn else LlamaAttention(config=config)
+        self.self_attn = LlamaAttention(config=config)
 
         self.mlp = LlamaMLP(
             hidden_size=self.hidden_size,
